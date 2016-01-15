3e79e30a9eb51cfe13b500cf10b552125e35f27d
AlonsoUchida <javier.uchida@valmar.com.pe>
2016-01-15 18:12:46 -0500

    .
    .

---
+++ Bonus.WebApiServices/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Bonus.WebApiServices.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Bonus.WebApiServices/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Bonus.WebApiServices.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
